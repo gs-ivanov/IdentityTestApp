698d442100c7b3b36e8a5d5e92a43d8e230cb3ea
George <gs.ivanov@abv.bg>
2025-03-18 12:13:42 +0200

    Test commit

--- Startup.cs
+++ Startup.cs
@@ -77,6 +77,7 @@ namespace IdentityTestApp
                     pattern: "{controller=Home}/{action=Index}/{id?}");
                 endpoints.MapRazorPages();
             });
+            // Test
         }
     }
 }
